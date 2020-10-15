7234fb124c6f8d5d3ab3be533ae619becbe4fca9
rahul.kohli@accionlabs.co <rahul.kohli@accionlabs.com>
2020-10-15 20:14:45 +0530

    performnce improvement  with lifecycle

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -3,20 +3,26 @@ import "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/aux";
 
-const Modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modelClosed}/>
-            <div
-                className="Modal"
-                style={{
-                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.show ? '1' : '0'
-                }}>
-                {props.children}
-            </div>
-        </Aux>
-    )
+class Modal extends React.Component {
+
+    shouldComponentUpdate(nextProps , nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modelClosed}/>
+                <div
+                    className="Modal"
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )    
+    }
 }
 
 export default Modal;
